2da7d7c8f0f880ef0fc60f4a2a5c083075c83c3f
jep <juepark42@gmail.com>
2025-04-16 14:43:13 +0900

    Initial Commit

---
+++ TaskTodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskTodo
+//
+//  Created by 박주은 on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskTodoTests/TaskTodoTests.swift
@@ -0,0 +1,16 @@
+//
+//  TaskTodoTests.swift
+//  TaskTodoTests
+//
+//  Created by 박주은 on 4/16/25.
+//
+
+import Testing
+
+struct TaskTodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
